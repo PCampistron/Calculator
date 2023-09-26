f97e74b4b7dce319ea1dd750c9ca809c2bc8baef
Pierre Campistron <pier.campistron@gmail.com>
2023-09-26 23:41:06 +0200

    Ajout de tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,13 @@
 package calculator;
 
 public class Calculator {
+    public int add(int opG, int opD)
+    {
+        return opG + opD;
+    }
 
+    public int divide(int opG, int opD)
+    {
+        return opG / opD;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,63 @@
 package calculator;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
+    private Calculator monCalculator;
 
+    @BeforeEach
+    void setup() { monCalculator = new Calculator(); }
+    @AfterEach
+    void tearDown() { monCalculator = null; }
+
+    @Test
+    void addition() {
+        // GIVEN
+        int nombre1 = 1;
+        int nombre2 = 2;
+
+        // WHEN
+        int resultat = monCalculator.add(nombre1, nombre2);
+
+        // THEN
+        assertThat(resultat).isEqualTo(3);
+    }
+
+    @Test
+    void division_resultat_entier() {
+        // GIVEN
+        int nombre1 = 4;
+        int nombre2 = 2;
+
+        // WHEN
+        int resultat = monCalculator.divide(nombre1, nombre2);
+
+        // THEN
+        assertThat(resultat).isEqualTo(2);
+    }
+
+    @Test
+    void division_resultat_decimal() {
+        // GIVEN
+        int nombre1 = 5;
+        int nombre2 = 3;
+
+        // WHEN
+        int resultat = monCalculator.divide(nombre1, nombre2);
+
+        // THEN
+        assertThat(resultat).isEqualTo(2);
+    }
+
+    @Test
+    void division_par_zero()
+    {
+
+    }
 }
