eb02fbebc2e697f432a6e921c1e174c45cd9a353
Pierre Campistron <92059222+PCampistron@users.noreply.github.com>
2023-09-27 12:32:38 +0200

    Implémentation test paramétré + exceptions

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,11 +3,20 @@ package calculator;
 public class Calculator {
     public int add(int opG, int opD)
     {
+        if(opG > 100 || opG < -100 || opD > 100 || opD < -100)
+        {
+            throw new IllegalArgumentException("Valeur entrée trop grande");
+        }
         return opG + opD;
     }
 
     public int divide(int opG, int opD)
     {
+        if(opD == 0)
+        {
+            throw new IllegalArgumentException("Division par zéro impossible");
+        }
+
         return opG / opD;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,15 +1,13 @@
 package calculator;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
     private Calculator monCalculator;
@@ -33,7 +31,7 @@ class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource( {"0, 1, 1", "1, 2, 3", "-2, 2, 0", "0, 0, 0", "-1, -2, -3"})
+    @CsvSource( {"0, 1, 1", "1, 2, 3", "-2, 2, 0", "0, 0, 0", "-1, -2, -3"} )
     void additionParametree(int opG, int opD, int resultatAttendu)
     {
         // GIVEN
@@ -45,6 +43,40 @@ class CalculatorTest {
 
         // THEN
         assertThat(resultat).isEqualTo(resultatAttendu);
+    }
+
+    @Test
+    void exception_addition_opG_trop_grand()
+    {
+        // GIVEN
+        int nombre1 = 101;
+        int nombre2 = 1;
+
+        try {
+            // WHEN
+            monCalculator.add(nombre1, nombre2);
+        } catch (Exception e)
+        {
+            // THEN
+            assertThat(e.getMessage()).isEqualTo("Valeur entrée trop grande");
+        }
+    }
+
+    @Test
+    void exception_addition_opD_trop_grand()
+    {
+        // GIVEN
+        int nombre1 = 1;
+        int nombre2 = 101;
+
+        try {
+            // WHEN
+            monCalculator.add(nombre1, nombre2);
+        } catch (Exception e)
+        {
+            // THEN
+            assertThat(e.getMessage()).isEqualTo("Valeur entrée trop grande");
+        }
     }
 
     @Test
@@ -76,6 +108,17 @@ class CalculatorTest {
     @Test
     void division_par_zero()
     {
+        // GIVEN
+        int nombre1 = 1;
+        int nombre2 = 0;
 
+        try {
+            // WHEN
+            monCalculator.divide(nombre1, nombre2);
+        } catch (Exception e)
+        {
+            // THEN
+            assertThat(e.getMessage()).isEqualTo("Division par zéro impossible");
+        }
     }
 }
