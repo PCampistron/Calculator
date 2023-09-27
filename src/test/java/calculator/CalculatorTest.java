872ce6ecf93a80c3f6fbf2e93e85bf5ca69abf2f
Pierre Campistron <92059222+PCampistron@users.noreply.github.com>
2023-09-27 11:31:24 +0200

    Ecriture test paramétré

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,6 +32,21 @@ class CalculatorTest {
         assertThat(resultat).isEqualTo(3);
     }
 
+    @ParameterizedTest
+    @CsvSource( {"0, 1, 1", "1, 2, 3", "-2, 2, 0", "0, 0, 0", "-1, -2, -3"})
+    void additionParametree(int opG, int opD, int resultatAttendu)
+    {
+        // GIVEN
+        int nombre1 = opG;
+        int nombre2 = opD;
+
+        // WHEN
+        int resultat = monCalculator.add(nombre1, nombre2);
+
+        // THEN
+        assertThat(resultat).isEqualTo(resultatAttendu);
+    }
+
     @Test
     void division_resultat_entier() {
         // GIVEN
@@ -46,7 +64,7 @@ class CalculatorTest {
     void division_resultat_decimal() {
         // GIVEN
         int nombre1 = 5;
-        int nombre2 = 3;
+        int nombre2 = 2;
 
         // WHEN
         int resultat = monCalculator.divide(nombre1, nombre2);
